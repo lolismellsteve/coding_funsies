6c7112638b17810bef1cfb1626d5cc0546f63213
lolismellsteve <sdoljansky@gmail.com>
2025-03-24 10:07:39 -0400

    something is wrong

--- example1.py
+++ example1.py
@@ -6,42 +6,39 @@ import random
 import sys
 
 
-user_input = "Enter either a number 1 or 2"
-print(user_input)
-user_input = int(input())
 
-
-if user_input == 1:
-    print("Hello World")
-
-if user_input == 2:
-    print("Hello Universe")
 
 #using this logic i could make something cool like what about a math test where
 #if you get one question right it says Good job and onto next question and 
 # if you get the next question wrong it generates a random number and you need to get three questions
 #right to pass
 counter = 0
-
-def function():
-    global counter
-    counter += 1
-    if counter == 3:
-        print("You passed")
-        sys.exit()
 random_number1 = random.randint(1, 100)
 random_number2 = random.randint(1, 100)
+user_input = int(input("What is " + str(random_number1) + " + " + str(random_number2) + "?"))
 
-
-print("What is " + str(random_number1) + " + " + str(random_number2) + "?")
-user_input = int(input())
-if user_input == random_number1 + random_number2:
-    print("Good job")
-else:
-    print("Try again")
     
-
-
-
+def function():
+    global counter
+    prompt = input("What is " + str(random_number1) + " + " + str(random_number2) + "?")
+    if prompt == random_number1 + random_number2:
+        counter += 1
+        print("Good job you have" + str(counter) + "points and you need to get three to win")
+        counter += 1
+        if counter == 3:
+            print("You passed")
+            sys.exit()
+    else:
+        print("Try again")
+        counter -= 1
+        print("You now have" + str(counter) + "points")
+        function()
 function()
-user_input = "What is"
+
+        
+
+        
+
+
+    
+    
